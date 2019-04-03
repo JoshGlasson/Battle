d27f7f8a16e3e828fc8b47c66dba212e2b6fc396
Josh Glasson <Josh.Glasson@tesco.com>
2019-04-03 15:43:14 +0100

    added turn switching, made multiplayer

--- spec/features/attack_spec.rb
+++ spec/features/attack_spec.rb
@@ -12,4 +12,22 @@ feature 'Attacking' do
     expect(page).not_to have_content "Sam: 60HP"
     expect(page).to have_content "Sam: 50HP"
   end
+
+    scenario 'reduce Player 1 HP by 10' do
+      sign_in_and_play
+      click_button "Attack"
+      click_button "OK"
+      click_button "Attack"
+      click_button "OK"
+      expect(page).not_to have_content "Josh: 60HP"
+      expect(page).to have_content "Josh: 50HP"
+    end
+
+  scenario 'can be attacked by player 2' do
+    sign_in_and_play
+    click_button "Attack"
+    click_button "OK"
+    click_button "Attack"
+    expect(page).to have_content 'Sam attacked Josh'
+  end
 end
