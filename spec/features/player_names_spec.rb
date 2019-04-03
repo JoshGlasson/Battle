efb363a1e7d035e057aef63dae326ec0e30bce3b
Josh Glasson <Josh.Glasson@tesco.com>
2019-04-03 14:29:56 +0100

    added web helper

--- spec/features/player_names_spec.rb
+++ spec/features/player_names_spec.rb
@@ -1,9 +1,6 @@
 feature 'Enter Players Names' do
   scenario 'Players enter their names' do
-    visit('/')
-    fill_in :player_1_name, with: "Josh"
-    fill_in :player_2_name, with: "Sam"
-    click_button 'Submit'
+    sign_in_and_play
     expect(page).to have_content 'Josh vs. Sam'
   end
 end
