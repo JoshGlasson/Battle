d76a675641d38b6e1be9ee06d0d1909ef62fb4cc
Josh Glasson <Josh.Glasson@tesco.com>
2019-04-03 14:56:19 +0100

    added game class

--- spec/player_spec.rb
+++ spec/player_spec.rb
@@ -2,10 +2,23 @@ require 'player'
 
 describe Player do
   subject(:josh) { Player.new('Josh') }
+  subject(:sam) { Player.new('Sam') }
 
   describe '#name' do
     it 'returns the name' do
       expect(josh.name).to eq 'Josh'
+    end
+  end
+
+  describe '#hit_points' do
+    it 'returns the hit points' do
+      expect(josh.hit_points).to eq described_class::DEFAULT_HIT_POINTS
+    end
+  end
+
+  describe '#receive_damage' do
+    it 'reduces the player hit points' do
+      expect { josh.receive_damage }.to change { josh.hit_points }.by(-10)
     end
   end
 end
