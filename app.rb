e706136b7b3f24e5efcedba432f03bdff0014afa
Josh Glasson <Josh.Glasson@tesco.com>
2019-04-03 16:27:27 +0100

    removed global var

--- app.rb
+++ app.rb
@@ -6,6 +6,10 @@ require './lib/attack'
 class Battle < Sinatra::Base
   enable :sessions
 
+  before do
+    @game = Game.instance
+  end
+
   get '/' do
     erb :index
   end
@@ -13,18 +17,18 @@ class Battle < Sinatra::Base
   post '/names' do
     player_1 = Player.new(params[:player_1_name])
     player_2 = Player.new(params[:player_2_name])
-    $game = Game.new(player_1, player_2)
+    @game = Game.create(player_1, player_2)
     redirect '/play'
   end
 
   get '/play' do
-    @game = $game
+    @game
     erb :play
   end
 
   post '/attack' do
-    Attack.run($game.opponent_of($game.current_turn))
-    if $game.game_over?
+    Attack.run(@game.opponent_of(@game.current_turn))
+    if @game.game_over?
       redirect '/game-over'
     else
       redirect '/attack'
@@ -32,19 +36,20 @@ class Battle < Sinatra::Base
   end
 
   get '/attack' do
-    @game = $game
+    @game
     erb :attack
   end
 
   post '/switch_turns' do
-    $game.switch_turns
-    redirect('/play')
+    @game.switch_turns
+    redirect '/play'
   end
 
   get '/game-over' do
-    @game = $game
+    @game
     erb :game_over
   end
+
   # start the server if ruby file executed directly
   run! if app_file == $0
 end
